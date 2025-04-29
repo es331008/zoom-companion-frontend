5581e03265f31db981634fe6d76d5c9980b4c17b
Evan <evan.sorger@gmail.com>
2025-04-29 12:09:40 -0400

    Fix env properties bug

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import {useEffect, useState} from "react";
-import {Navigate} from "react-router";
+import {Navigate, useNavigate} from "react-router";
 import {Button, CircularProgress, Container, List, ListItem, ListItemText, Typography} from "@mui/material";
 import {IMeeting} from "../interfaces/IMeeting.tsx";
 
@@ -8,7 +8,7 @@ const Home = () => {
     const [meetings, setMeetings] = useState<IMeeting[] | null>(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState<string | null>(null);
-    // const navigate = useNavigate();
+    const navigate = useNavigate();
     const zoomCompanionAuthServiceUri = import.meta.env.VITE_ZOOM_COMPANION_AUTH_SERVICE_URI;
     const zoomCompanionApiServiceUri = import.meta.env.VITE_ZOOM_COMPANION_API_SERVICE_URI;
 
@@ -31,27 +31,9 @@ const Home = () => {
             .finally(() => setLoading(false));
     }, []);
 
-    // const navigateToMeeting = (meetingId: number) => {
-    //     navigate(`/companion/${meetingId}`)
-    // }
-
-    const joinMeeting = async (meetingId: number) => {
-        console.log("Attempting to join meeting: " + meetingId)
-        try {
-            const response = await axios.post("http://localhost:5002/api/join_meeting", {
-                meetingId: meetingId,
-                role: 0
-            }, {
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            })
-
-            return response.data.signature
-        } catch (e) {
-            console.log(e);
-        }
-    };
+    const navigateToMeeting = (meetingId: number) => {
+        navigate(`/companion/${meetingId}`)
+    }
 
     return meetings ? (
         <Container maxWidth="md" style={{ marginTop: "50px", textAlign: "center" }}>
@@ -73,7 +55,7 @@ const Home = () => {
                                 primary={meeting.topic}
                                 secondary={`Meeting ID: ${meeting.id}`}
                             />
-                            <Button variant="contained" color="primary" onClick={() => joinMeeting(meeting.id)}>
+                            <Button variant="contained" color="primary" onClick={() => navigateToMeeting(meeting.id)}>
                                 Request Companion
                             </Button>
                         </ListItem>
